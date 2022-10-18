7bc59d36615cfb1f672fe2099a884a33210782ba
matthewcheng222 <38536795+matthewcheng222@users.noreply.github.com>
2022-10-18 14:50:50 +0000

    Deploying to public from @ matthewcheng222/portfolio@9920b4f13cc97806cd2bb6e1c4021c69dadc870f 🚀

--- _next/static/46vrqGvLKLGyz4B7wzuhC/_buildManifest.js
+++
@@ -1,1 +0,0 @@
-self.__BUILD_MANIFEST={__rewrites:{beforeFiles:[],afterFiles:[],fallback:[]},"/":["static/css/7c9e31c2156353a4.css","static/chunks/pages/index-390cd69ca5f42d9a.js"],"/_error":["static/chunks/pages/_error-a4ba2246ff8fb532.js"],sortedPages:["/","/_app","/_error"]},self.__BUILD_MANIFEST_CB&&self.__BUILD_MANIFEST_CB();
---
+++ _next/static/depdz3cc4zIT77AVNZuGj/_buildManifest.js
@@ -0,0 +1,1 @@
+self.__BUILD_MANIFEST={__rewrites:{beforeFiles:[],afterFiles:[],fallback:[]},"/":["static/chunks/0c428ae2-bcf0ba6d4f231caf.js","static/chunks/pages/index-8bde07ebb42b08b4.js"],"/_error":["static/chunks/pages/_error-a4ba2246ff8fb532.js"],sortedPages:["/","/_app","/_error"]},self.__BUILD_MANIFEST_CB&&self.__BUILD_MANIFEST_CB();

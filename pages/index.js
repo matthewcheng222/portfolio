7fe32a9fdacdb8c1ecf75d115dbc0caba370db03
Matthew Cheng <matthewcheng222@gmail.com>
2022-10-18 21:25:00 +0100

    update new layout

--- pages/index.js
+++ pages/index.js
@@ -1,5 +1,6 @@
 import Head from 'next/head'
-import {BsFillMoonStarsFill} from 'react-icons/bs'
+import Image from 'next/image'
+import Navbar from '../components/Navbar'
 
 export default function Home() {
   return (
@@ -10,22 +11,7 @@ export default function Home() {
         <link rel="icon" href="/favicon.ico" />
       </Head>
 
-      <main className="px-10">
-        <section className="bg-white min-h-screen">
-          <nav className="py-10 mb-12 flex justify-between">
-            <h1 className="text-xl">Matthew Cheng</h1>
-            <ul className="flex items-center">
-              <li>
-                <BsFillMoonStarsFill className="cursor-pointer text-2xl"/>
-              </li>
-              <li>
-                <a className="bg-gradient-to-tr from-cyan-500 to-purple-500 text-white px-4 py-2 rounded-md ml-8" href="#">Resume</a>
-              </li>
-            </ul>
-          </nav>
-          
-        </section>
-      </main>
+      <Navbar />
     </div>
   )
 }
